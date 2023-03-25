c6fcd4e010a94ccafcee8d04526c54a6f35e41db
Maksim Komkov <KomkovMS@yandex.ru>
2023-03-25 08:15:44 +0700

    ДЗ-2 задача 1 добавил эталонное решение

--- Hw_2/Hw_2_1.py
+++ Hw_2/Hw_2_1.py
@@ -23,5 +23,22 @@ for el in coin:
 
 print(f'На столе лежит n = {len(coin)} монеток,\n'
       f'из них вверх решкой {count_0},вверх гербом {count_1},\n'
-      f'Минимальное количество монет, которое'
+      f'Минимальное количество монет, которое '
       f'нужно перевернуть {min_coin}')
+
+# Эталонное решение
+
+n = int(input('Введите количество монет: '))
+count_zero = 0
+count_one = 0
+
+for i in range(n):
+    x = int(input('Введите положение монетки: '))
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
+else:
+    print(count_one)
