c6a463f73feb505ff261eb9f83123286f622647b
Maksim Komkov <KomkovMS@yandex.ru>
2023-03-29 20:44:57 +0700

    Выполнил ДЗ-4 задание 1 (добавил Эталонное решение)

--- Hw_4/Hw_4_1.py
+++ Hw_4/Hw_4_1.py
@@ -44,3 +44,32 @@ for n in my_lst_1:
 my_lst_res = sorted(set(my_lst_res))
 
 print(my_lst_res)
+
+# Эталонное решение:
+
+mol = [int(x) for x in input().split()]
+n = mol[0]
+m = mol[1]
+
+set_1 = set()
+set_2 = set()
+
+list_1 = list()
+
+a = [int(x) for x in input().split()]
+k = set(a)
+
+for i in k:
+    set_1.add(i)
+
+b = [int(x) for x in input().split()]
+k1 = set(b)
+
+for i in k1:
+    set_2.add(i)
+
+lok = set_1 & set_2
+kool = list(lok)
+kool.sort()
+for i in kool:
+    print(i, end=' ')
