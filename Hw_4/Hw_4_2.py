521cc550279b1aca97cb347a4ef9a4671debe616
Maksim Komkov <KomkovMS@yandex.ru>
2023-03-29 21:09:49 +0700

    Выполнил ДЗ-4 задание 2 (добавил Эталонное решение)

--- Hw_4/Hw_4_2.py
+++ Hw_4/Hw_4_2.py
@@ -34,3 +34,17 @@ print(
     f'максимальное число ягод,которое\nможет собрать за один заход собирающий '
     f'модуль: {max_res}'
 )
+
+# Эталонное решение:
+
+n = int(input())
+arr = list()
+for i in range(n):
+    x = int(input())
+    arr.append(x)
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
