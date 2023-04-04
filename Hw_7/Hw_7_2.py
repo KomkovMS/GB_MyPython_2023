48c802a4f6380969cb0605e8400b7b338041aae9
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-04 20:43:44 +0700

    Выполнил ДЗ-7 задание 2.

--- Hw_7/Hw_7_2.py
+++ Hw_7/Hw_7_2.py
@@ -17,3 +17,24 @@ length (длина в метрах), width (ширина в метрах).
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
 
+
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.thickness = 0.05
+
+    def mass_calculation(self):
+        mass = self._length * self._width * self.weight * self.thickness
+        return int(mass)
+
+
+my_obj = Road(5000, 20)
+mass_calc = my_obj.mass_calculation()
+print(type(mass_calc))
+print(
+    f'Масса асфальта, необходимого для покрытия всего дорожного полотна: '
+    f'{my_obj._length}м*{my_obj._width}м*{my_obj.weight}кг*'
+    f'{my_obj.thickness}м = {my_obj.mass_calculation()} кг = '
+    f'{int(my_obj.mass_calculation()/1000)} тонн.')
